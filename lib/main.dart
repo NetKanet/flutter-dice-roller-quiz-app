dcb2ee8ddc9f5be00245ea371a584d18bbcb5d9b
Kanet.kam <>
2026-04-23 23:57:28 +0700

    feat: add backgroundColor screen and delete test file

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 18, 227, 21),
         body: Center(
           child: Text("Hello World"),
         ),
