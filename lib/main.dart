9d1122a4ebf4c7d8f90e3a845d23d3844e60a887
Kanet.kam <>
2026-04-24 00:13:36 +0700

    feat: add gradient background to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,18 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 18, 227, 21),
-        body: Center(
-          child: Text("Hello World"),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 75, 233, 115),
+                Color.fromARGB(255, 129, 243, 211),
+              ],
+            ),
+          ),
+          child: const Center(child: Text("Hello World")),
         ),
       ),
-    )
+    ),
   );
 }
